43c5f16658e5c58b09c0f3a35ce0d53b43c75605
Manu Jiménez Bernal <manu.asi2@gmail.com>
2016-03-29 14:51:36 +0200

    1.0

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ var async = require("async");
 var places=[];
 var resultados=[];
 var BigNumber = require('big-number');
-
+var acabose=[];
 function peticion(lugar,url,callback){
     request({
-    url: url,
-    headers: {
-    //'Content-Length': contentLength,
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0'
-    },
-    port: 80,
-    json: true
+        url: url,
+        headers: {
+        //'Content-Length': contentLength,
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0'
+        },
+        port: 80,
+        json: true
     }, function (error, response, body) {
     if (!error) {
             var sum=0,medias=0;
@@ -25,7 +25,7 @@ function peticion(lugar,url,callback){
             var fin;
           
            if(resp){
-            resp.forEach(function(item){
+            async.each(resp,function(item){
                 if(item.size!=''){
                     medias+=Math.round((item.price)/(item.size));
                 }
@@ -35,7 +35,7 @@ function peticion(lugar,url,callback){
             //console.log("lugar-> "+lugar+" resultados[lugar] "+resultados[lugar]);
             }
       
-            callback(resultados);
+            return callback(resultados);
     }
     else
             console.log(error);
@@ -58,23 +58,28 @@ lr.on('end', function () {
         var escaped_str = require('querystring').escape(item);
         //console.log(item);
         var index=1;
-        while(index<=5){
-                    var url = "http://api.nestoria.es/api?action=search_listings&country=es&encoding=json&listing_type=buy&page="+index+"&place_name="+escaped_str+"&pretty=1&number_of_results=50";
+        //console.log("entrando while");
+        while(index<=10){
+           // console.log("he entrado en while");
+                    var url = "http://api.nestoria.es/api?action=search_listings&country=es&encoding=json&listing_type=buy&page="+index+"&place_name="+escaped_str+"&pretty=1&number_of_results=100";
                     peticion(item,url,function(resu){
-                        console.log(resultados[item]);
-                        resultados[item]=resultados[item]/50;
-                        console.log("resultado -> "+item+" "+resultados[item] );
+                        //console.log("peticion "+index);
+                      //  console.log(resultados[item]);
+                        resultados[item]+=resu[item]/50;
+                        //console.log("resultado -> "+item+" "+resultados[item] );
+                        //resultados[item]+=
                         //acabose=calculaMedia(resu);
-
+                        
                     });
-           index++;
+        resultados[item]=resultados[item]/10;
+        acabose[item]=resultados[item];
+        index++;
         }
-       
+        console.log(acabose[item]+" - "+item);
+      // console.log("saliendo while");
     },function(err){
-
         console.log(err);
-    });
-        
+    });     
 });
 
 
