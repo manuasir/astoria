deb43ec205dafc3019c5b38399ec4f6ad6dccb70
Manu Jiménez Bernal <manu.asi2@gmail.com>
2016-04-01 12:04:10 +0200

    1.0

--- index.js
+++ index.js
@@ -5,10 +5,28 @@ var LineByLineReader = require('line-by-line');
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('../nestoria.db');
 var places=[];
-
+var async = require('async');
 var check;
 
-function peticion(lugar,url,callback){
+
+
+function peticionAsincrona(url,salida,callback){
+    console.log("PETICION ASINCRONA");
+async.parallel([
+function(callback){
+    peticion(url,salida,function(){
+        callback();
+    });
+}
+    ]);
+console.log("FIN PETICION ASINCRONA");
+}
+
+
+
+function peticion(url,salida,callback){
+
+   
     
     request({
         url: url,
@@ -18,15 +36,18 @@ function peticion(lugar,url,callback){
         },
         port: 80,
         json: true
-        }, function (error, response, body) {
+        }, function (error, response,body,callback) {
+            console.log("peticion");
         if (!error) {
+            //console.log("callback");
             var sum=0,medias=0;
             var respuesta=body.response;
             var fin;
             if(respuesta){
                 var resp=respuesta.listings;
                 if(resp){
-                resp.forEach(function(item){
+                async.each(resp,function(item){
+                    console.log("introduciendo en bd");
                     if(item.size!=''){
                         db.serialize(function() {
                             var stmt = db.prepare("INSERT INTO casas VALUES (?,?,?,?,?,?,?)");
@@ -34,19 +55,27 @@ function peticion(lugar,url,callback){
                             stmt.run(item.guid,item.price,item.size,item.longitude,item.latitude,item.title,item.property_type);
                             stmt.finalize();
                         });
+                        //callback();
                     }
+                },function(){
+                    console.log("OTRA PAGINA");
+                    callback();
                 });
                 }
+                //callback();
             }
+            //callback();
         }
         else{
-                //console.log("errorcillo ->"+error);
+                console.log("error ->"+error);
                 //setTimeout(function(){},10);
-                return callback(error);
+                callback(error);
             }
     
     });
-return callback();
+//return callback();
+
+console.log("fin peticion");
 }
 
 function handleFile(file,callback){
@@ -67,29 +96,42 @@ function handleFile(file,callback){
     });
 }
 
+
 function procesa(){
     console.log("procesando...");
-    places.forEach(function(item){
+    async.each(places,function(item,next1){
+        console.log("un sitio: "+item);
         var escaped_str = require('querystring').escape(item);
         var err=false;
         var index=1;
+        paginar(escaped_str,next1);
         
-        while(index<=20 && !err){
-            var url = "http://api.nestoria.es/api?action=search_listings&country=es&encoding=json&listing_type=buy&page="+index+"&place_name="+escaped_str+"&pretty=1&number_of_results=50";
-            peticion(item,url,function(error){
-                index++;
-                if(error){
-                    err=true;
-                    console.log("error en callback "+error);
-                }
-            });
-        }
+        
     },function(){
+        console.log("municipios terminados");
+    });
+}
 
-    }
+
+function paginar(escaped_str,next2){
+
+
+async.times(20, function(n, next2){
+    var url = "http://api.nestoria.es/api?action=search_listings&country=es&encoding=json&listing_type=buy&page="+n+"&place_name="+escaped_str+"&pretty=1&number_of_results=50";
+    console.log("pagina-> "+n+" sitio: "+escaped_str);
+    paginaUnaVez(url,next2);
+    }, function(err, users) {
+         console.log("fin de paginas");
+    });
+}
+
+function paginaUnaVez(url, next3){
+   console.log("enviando peticion...");
+    peticion(url,function(){
+        next3();
+    });
 }
 
 handleFile('places.txt',function(){
     procesa();
 });
-
