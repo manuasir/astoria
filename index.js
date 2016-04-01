09f390a391eadb238e1a62ce0e3768944b80cc4d
Manu Jiménez Bernal <manu.asi2@gmail.com>
2016-04-01 14:36:33 +0200

    1.0

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var cpaginacion=1
 
 
 function peticion(url,str,i,c){
-   
+   console.log("relizando nueva peticion.........."+c);
     
     request({
         url: url,
@@ -30,7 +30,7 @@ function peticion(url,str,i,c){
             
             
         if (!error) {
-            console.log("procesando. i: "+i+" c: "+c);
+            console.log("procesando. sitio: "+places[i]+" pagina: "+c);
             //console.log("callback");
             var sum=0,medias=0;
             var respuesta=body.response;
@@ -38,40 +38,40 @@ function peticion(url,str,i,c){
             if(respuesta){
                 var resp=respuesta.listings;
                 if(resp){
-                async.each(resp,function(item){
-                    console.log("introduciendo en bd");
+                resp.forEach(function(item){
+                    
                     if(item.size!=''){
+                        //console.log("introduciendo en bd");
                         // db.serialize(function() {
                         //     var stmt = db.prepare("INSERT INTO casas VALUES (?,?,?,?,?,?,?)");
-                        //     console.log(item.guid,item.price,item.size,item.longitude,item.latitude,item.property_type);
+                             console.log(item.price,item.size,item.longitude,item.latitude,item.property_type);
                         //     stmt.run(item.guid,item.price,item.size,item.longitude,item.latitude,item.title,item.property_type);
                         //     stmt.finalize();
                         // });
                         //callback();
+
                     }
-                },function(){
-                    console.log("OTRA PAGINA");
-                    //callback();
+                    
                 });
+                console.log("fin peticion "+c);
+                        c++;
+                        paginar(str,i,c,false);
                 }
               
             }
-        
+            else{
+                console.log("no hay datos");
+            }
         }
         else{
-                console.log("error ->"+error);
+                console.log("error!!!!!!! ->"+error+ " volviendo a hacer la peticion "+c);
+                paginar(str,i,c,true);
               
             }
-    
-    
-    
-
-    c++;
-    console.log("fin peticion "+c);
-    paginar(str,i,c);
-    });
-//return callback();
-
+        
+           
+        
+        });
 
 }
 
@@ -94,14 +94,14 @@ function handleFile(file,callback){
 }
 
 function procesa(i){
-    console.log("procesando...");
+    //console.log("procesando...");
     if(i<places.length){
     var item=places[i];
-    console.log("un sitio: "+item);
+    //console.log("un sitio: "+item);
         var escaped_str = require('querystring').escape(item);
         var err=false;
         var index=1;
-        paginar(escaped_str,i, cpaginacion);
+        paginar(escaped_str,i, cpaginacion,false);
     }
     else{
         console.log("fin de pueblos");
@@ -123,21 +123,23 @@ function procesa(i){
 }
 
 
-function paginar(escaped_str,i, cpaginacion){
+function paginar(escaped_str,i, cpaginacion,error){
 
 
 //async.times(20, function(n, next2){
     var url = "http://api.nestoria.es/api?action=search_listings&country=es&encoding=json&listing_type=buy&page="+cpaginacion+"&place_name="+escaped_str+"&pretty=1&number_of_results=50";
-    console.log(url);
-    console.log("pagina-> "+cpaginacion+" sitio: "+escaped_str);
+    var ukurl = "http://api.nestoria.co.uk/api?action=search_listings&country=uk&encoding=json&listing_type=buy&page="+cpaginacion+"&place_name="+escaped_str+"&pretty=1&number_of_results=50";
+   
+   // console.log(url);
+    //console.log("pagina-> "+cpaginacion+" sitio: "+escaped_str);
    // peticion(url,next2);
   //  }, function(err, users) {
     //     console.log("fin de paginas");
-    if(cpaginacion==20){
+    if(cpaginacion==20 && !error){
         i++;
         procesa(i);
     }else {
-        peticion(url, escaped_str,i, cpaginacion);
+        peticion(ukurl, escaped_str,i, cpaginacion);
     }
    // });
 }
@@ -147,7 +149,7 @@ function paginar(escaped_str,i, cpaginacion){
 //     peticion(url,next3);
 // }
 
-handleFile('places.txt',function(){
+handleFile('placesuk.txt',function(){
     index=0;
     procesa(index);
 });
