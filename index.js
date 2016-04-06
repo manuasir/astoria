1bdf528a002549e9feb5d20c6859beb3043825e4
Manu Jiménez Bernal <manu.asi2@gmail.com>
2016-04-06 14:30:29 +0200

    Ventas y alquileres

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// Manuel Jiménez Bernal - Iliberi S.A. Granada 2016
-var str2json = require('string-to-json');
+
 var request = require("request");
 var LineByLineReader = require('line-by-line');
 var sqlite3 = require('sqlite3').verbose();
@@ -10,30 +9,15 @@ var check;
 var index;
 var cpaginacion=1;
 var cambio=false;
+var contador=0;
 
-function insertData(guid,price,size,longitude,latitude,text,property_type,callback){
+function insertData(c,price,size,longitude,latitude,property_type,callback){
  db.serialize(function() {
-  var stmt = db.prepare("INSERT INTO casas VALUES (?,?,?,?,?,?,?)");
-  stmt.run(guid,price,size,longitude,latitude,text,property_type);
- },function(){
-    console.log("fin transaccion");
+  var stmt = db.prepare("INSERT INTO casas VALUES (?,?,?,?,?,?)");
+  stmt.run(c,price,size,longitude,latitude,property_type,function(){
+   // console.log("fin transaccion");
     callback();
- });
-}
-
-function insertDataSec(guid,longitude,latitude,callback){
-   // console.log("altero en bd");
- db.serialize(function() {
-  //var stmt = db.prepare("INSERT INTO casas_copy(guid,longitude,latitude) VALUES (?,?,?)");
-  var stmt = db.prepare("UPDATE casas SET longitude= ?,latitude=? WHERE guid=?");
-  stmt.run(longitude,latitude,guid,function(err){
-        if(err){
-            callback(err);
-        }
-  });
- },function(){
-    console.log("fin transaccion");
-    callback();
+});
  });
 }
 
@@ -58,11 +42,16 @@ function peticion(url,str,i,c){
             var fin;
             if(respuesta){
                 var resp=respuesta.listings;
-                if(resp){
+
+                if(resp && resp.length>1){
+                 //console.log(resp.length);
                 resp.forEach(function(item,next){
                 var remaining = resp.length;
-                    if(item.size!=''){
-                        insertData(item.guid,item.size,item.price,item.longitude,item.latitude,item.title,item.property_type,function(){                    
+
+                    if(item.size>0 && item.price){
+                       //console.log(item.hasOwnProperty('price'));
+                       //console.log(item.listing_type);
+                        insertData(c,item.price,item.latitude,item.longitude,item.size,item.listing_type,function(){                    
                             if (!--remaining){ //console.log("nueva entrada"); 
                                 next();
                             }
@@ -72,92 +61,26 @@ function peticion(url,str,i,c){
                 });
                 //console.log("fin peticion "+c);
                         c++;
-                        paginar(str,i,c,false);
-                }
-                else{
-                    //console.log("NO RESP");
-                    paginar(str,i,c,true);
-                }
-              
-            }
-            else{
-                //console.log("no hay datos");
-            }
-        }
-        else{
-                console.log("error!!!!!!! ->"+error+ " volviendo a hacer la peticion "+c);
-                paginar(str,i,c,true);
-              
-            }
-        
-           
-        
-        });
-}
-
-function peticion2(url,str,i,c){
-   //console.log("realizando nueva peticion.........."+c);
-    
-    request({
-        url: url,
-        headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0'
-        },
-        port: 80,
-        json: false
-        }, function (error, response,body) {
-            
-        if (!error) {
-            //console.log("callback");
-            var sum=0,medias=0;
-            var respuesta=body;
-            var fin;
-            if(respuesta){
-                //var resp=JSON.stringify(respuesta);
-                //var resp = str2json.convert(respuesta);
-                var resp=JSON.parse(respuesta);
-               // console.log(resp);
-                if(resp.total>0){
-                    //console.log("resp->"+resp);
-                   // console.log("tipo ->"+Object.prototype.toString.call(resp));
-                //resp.forEach(function(item,next){
-                async.forEach(resp,function(item,next){
-
-                var remaining = resp.length;
-                    if(item.location){
-                        //console.log(item.location.lon);
-                        insertDataSec(item.guid,item.location.lon,item.location.lat,function(){                    
-                            if (!--remaining){ //console.log("nueva entrada"); 
-                                next();
-                            }
-                        });
-                    }
-                    
-                });
-               // });
-                console.log("fin peticion "+c);
-                        c+=100;
-                        paginarsec(str,i,c,false);
+                        paginar(str,i,c,false,cambio);
                 }
                 else{
                     console.log("NO RESP");
-                    paginarsec(str,i,c,true);
+                    paginar(str,i,c,true,cambio);
                 }
               
             }
             else{
                 console.log("no hay datos");
-                paginarsec(str,i,c,true);
             }
         }
         else{
                 console.log("error!!!!!!! ->"+error+ " volviendo a hacer la peticion "+c);
-                paginarsec(str,i,c,true);
+                paginar(str,i,c,true,cambio);
             }
 
         });
 }
+
 
 function handleFile(file,callback){
     var lr = new LineByLineReader(file);
@@ -182,17 +105,18 @@ function procesa(i){
     case false:
         if(i<places.length){
         var item=places[i];
-        console.log("un sitio: "+item);
+        
             var escaped_str = require('querystring').escape(item);
+            console.log("un sitio: "+escaped_str);
             var err=false;
             var index=1;
             //cpaginacion=1;
-            paginar(escaped_str,i, cpaginacion,false);
+            paginar(escaped_str,i, cpaginacion,cambio);
         }
         else{
             console.log("fin de pueblos");
             cambio=true;
-            cpaginacion=0;
+            //cpaginacion=0;
             procesa(0);
             //paginarsec();
             //peticion2();
@@ -201,17 +125,18 @@ function procesa(i){
     case true:
         if(i<places.length){
         var item=places[i];
-        console.log("un sitio en segunda vuelta: "+item);
-            var escaped_str = require('querystring').escape(item);
-            var err=false;
-            var index=1;
+        
+        var escaped_str = require('querystring').escape(item);
+        console.log("un sitio para comprobar tipo RENT: "+escaped_str);
+            //var err=false;
             //cpaginacion=1;
-            paginarsec(escaped_str,i, cpaginacion,false);
+            //cpaginacion=1;
+            paginar(escaped_str,i, cpaginacion,false,cambio);
         }
         else{
             console.log("fin de pueblos");
-            cambio=true;
-            cpaginacion=1;
+            
+            
             //paginarsec();
             //peticion2();
         }
@@ -219,13 +144,17 @@ function procesa(i){
     } 
 }
 
-function paginar(escaped_str,i, cpaginacion,error){
+function paginar(escaped_str,i, cpaginacion,error,cambio){
 
     //console.log("entro en paginar. cpaginacion= "+cpaginacion+" error "+error);
-    var url = "http://api.nestoria.es/api?action=search_listings&country=es&encoding=json&listing_type=buy&page="+cpaginacion+"&place_name="+escaped_str+"&pretty=1&number_of_results=50";
+    if(!cambio)
+        var url = "http://api.nestoria.es/api?action=search_listings&country=es&encoding=json&listing_type=buy&page="+cpaginacion+"&place_name="+escaped_str+"&pretty=1&number_of_results=50&bedroom_min=1&bedroom_max=30";
+    else
+        var url = "http://api.nestoria.es/api?action=search_listings&country=es&encoding=json&listing_type=rent&page="+cpaginacion+"&place_name="+escaped_str+"&pretty=1&number_of_results=50";
+    
     //var ukurl = "http://api.nestoria.co.uk/api?action=search_listings&country=uk&encoding=json&listing_type=buy&page="+cpaginacion+"&place_name="+escaped_str+"&pretty=1&number_of_results=50";
    // var securl = "http://172.17.4.14/nestoria.php?location="+escaped_str+"&listing_type=buy&property_type=property&offset="+cpaginacion;
-    if(cpaginacion==21 || error){
+    if(cpaginacion==340 || error){
         console.log("terminado sitio: "+escaped_str+" paginas: "+cpaginacion);
         i++;
         procesa(i);
@@ -237,26 +166,8 @@ function paginar(escaped_str,i, cpaginacion,error){
     }
 }
 
-function paginarsec(escaped_str,i, cpaginacion,error){
-
-    //console.log("entro en paginar. cpaginacion= "+cpaginacion+" error "+error);
-   // var url = "http://api.nestoria.es/api?action=search_listings&country=es&encoding=json&listing_type=buy&page="+cpaginacion+"&place_name="+escaped_str+"&pretty=1&number_of_results=50";
-   // var ukurl = "http://api.nestoria.co.uk/api?action=search_listings&country=uk&encoding=json&listing_type=buy&page="+cpaginacion+"&place_name="+escaped_str+"&pretty=1&number_of_results=50";
-    var securl = "http://172.17.4.14/nestoria.php?location="+escaped_str+"&listing_type=buy&property_type=property&offset="+cpaginacion;
-    if(cpaginacion==1100 || error){
-        console.log("terminado sitio: "+escaped_str+" paginas: "+cpaginacion);
-        i++;
-        procesa(i);
-    }else{
-        //console.log("no he llegado al fin de paginas,hago otra peticion "+cpaginacion);
-        peticion2(securl, escaped_str,i, cpaginacion);
-        //peticion2(securl, escaped_str,i, cpaginacion);
-
-    }
-}
 
 handleFile('places.txt',function(){
     index=0;
     procesa(index);
 });
-
